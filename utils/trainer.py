20d6936de54b22cf3d767fc4898aecb4e1d3fd7c
Taya-san <proriesya07@gmail.com>
2026-02-04 16:04:37 +0700

    update trainer

--- utils/trainer.py
+++ utils/trainer.py
@@ -1,31 +1,77 @@
 from dekm_functions import run_kmeans, compute_sw, compute_eigen
 import warnings
 from torch.utils.data import DataLoader
+import torch.optim as optim
 from datasets import Dataset
+from tqdm import tqdm
+
 try:
     import mamba_ssm
 except:
     warnings.warn('missing dependencies likely to be mamba_ssm or causal-conv1d', UserWarning)
 
-def train_modelnoclt(model, epoch, optimizer, data, tr_data, ts_data, device, batch_size):
-    model.to(device)
+def train_modelnoclt(
+        model,
+        epochs,
+        optimizer_type,
+        data = None,
+        tr_data,
+        ts_data,
+        device = 'cuda',
+        batch_size,
+        epochs_loss_log = None
+        ):
 
-    if not isinstance(tr_data, Dataset):
-        train_data = Dataset.from_dict({
-            "text":tr_data
-            })
+    if data != None :
+        training_data = data['train']
+        test_data = data['test']
     else:
-        train_data = tr_data
-
-    if not isinstance(ts_data, Dataset):
-        test_data = Dataset.from_dict({
-            "text":ts_data
-            })
-    else:
+        training_data = tr_data
         test_data = ts_data
 
-    
+    training_loader = DataLoader(
+            training_data,
+            batch_size = batch_size,
+            shuffle = True,
+            num_workers = 2,
+            pin_memory = True
+    )
 
-    
+    test_loader = DataLoader(
+            test_data,
+            batch_size = batch_size,
+            shuffle = True,
+            num_workers = 2,
+            pin_memory = True
+    )
 
+    if optimizer_type == 'adam':
+        optimizer = optim.Adam(model.parameters(), lr = 1e-3)
+    elif optimizer_type == 'sgd':
+        optimizer = optim.SGD(model.parameters(), lr = 1e-3)
+
+    model.train()
+    model.float()
+    model.to(device)
+
+    if epochs_loss_log != None:
+        epochs_loss = epochs_loss_log
+    else:
+        epochs_loss = []
+
+    for epoch in range(epochs):
+        running_loss = 0.0
+
+        loop = tqdm(train_loader, desc=f'Epoch {epoch + 1}/epochs}', leave=True)
+
+        for batch in loop:
+            outputs = model(input_ids = loop['input_ids'],
+                            attention_mask = loop['attention_mask'],
+                            labels = loop['labels']
+            )
+
+
+                    
+
+            
 
